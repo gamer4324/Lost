b95342f13917e123ba4265c5305be7607e7cafc2
gamer4324 <brodey.the.cat@gmail.com>
2022-10-23 21:14:01 -0300

    s

--- script.js
+++ script.js
@@ -568,8 +568,8 @@ function makeRoom(name, position, exitDoors = [1,2], entrenceDoors = [3,4]) {
 	let Rect = new rect(new vector2(roomSize.x*position.x,roomSize.y*position.y),new vector2(roomSize.x,roomSize.y))
 	let decorations = []
 	for (let V = 0; V <= randInt(0,3); V++) {
-		let size = vector2(randInt(roomSize.x/randInt(0,40)/100),randInt(roomSize.y/randInt(0,40)/100))
-		let pos = vector2(randInt(roomSize.x/randInt(0,100)/100),randInt(roomSize.y/randInt(0,100)/100))
+		let size = new vector2(randInt(roomSize.x/randInt(0,40)/100),randInt(roomSize.y/randInt(0,40)/100))
+		let pos = new vector2(randInt(roomSize.x/randInt(0,100)/100),randInt(roomSize.y/randInt(0,100)/100))
 		decorations.push(-1,pos,size)
 	}
 	let roomData = [decorations, exitDoors, entrenceDoors]
